8902160bd719e857bdda14e481186f750d63d003
Claire Watson <watson.e.claire@gmail.com>
2018-02-01 14:33:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: "songTitle"}
 
 function updatePlaylist (artistName, "songTitle"){
-return Object.assign({}, playlist, {artistName: "playlist"})
+return Object.assign({}, playlist, {[artistName]: "playlist"})
 }
